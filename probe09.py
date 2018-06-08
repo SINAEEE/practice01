b4fc8e05e52c24cfcca28164bf287c60f1d8546d
SINAEEE <kimshin1227@naver.com>
2018-06-08 17:14:29 +0900

    test

---
+++ probe09.py
@@ -0,0 +1,20 @@
+
+
+str1 = input("입력 > ")
+
+result=str1[::-1]
+print(result)
+
+"""
+str1 = input("입력 > ")
+
+lst=[]
+
+for i in str1:
+    lst.append(i)
+
+lst.reverse()
+
+print("결과 > ",)
+"""
+
