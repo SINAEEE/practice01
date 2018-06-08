932561947deaab906d960094eb3f36c41dd126f8
SINAEEE <kimshin1227@naver.com>
2018-06-08 15:52:23 +0900

    test

--- probe08.py
+++ probe08.py
@@ -4,7 +4,13 @@ lst = []
 print("5개의 정수를 입력하세요 : ")
 for i in range(0,5):
     lst.append(input())
-    lst[i] = num(lst[i])
 
+t=0
+for i in lst:
+    i = int(i)
+    t += i
 
-print(lst)
+avg = t/5
+
+print("리스트 : ",lst)
+print("평균 : ",avg)
