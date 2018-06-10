c60764ce7174976eaedc06dc138171120ea05659
SINAEEE <kimshin1227@gmail.com>
2018-06-10 17:29:59 +0900

    probe commit!

--- probe05_2.py
+++ probe05_2.py
@@ -1,25 +1,21 @@
 
 
+
 s = """We encourage everyone to contribute to Python. If you still have questions after reviewing the material
 in this guide, then the Python Mentors group is available to help guide new contributors through the process."""
 
 print()
 result = s.upper().replace(',','').replace('.','').replace('\n','').split()
 
-s=set(result)
-r2=sorted(s)
+r2=sorted(result)
+
+d={}
 
 for i in r2:
-    print(i)
+    if i in d.keys():
+        d[i] += 1
+    else:
+        d[i] = 1
 
-print()
-
-
-
-
-
-
-
-
-
-
+for k,v in d.items():
+    print("{}:{}".format(k,v))
--- probe13.py
+++ probe13.py
@@ -6,14 +6,14 @@ num=int(num)
 r=0
 if num%2==0:
     n=num+1
-    for i in range(1,n,1):
+    for i in range(1,n):
         #print(i)
         if i%2==0:
            r += i
     print("결과 값 : ",r)
 else:
     n=num+1
-    for i in range(1,n,1):
+    for i in range(1,n):
         #print(i)
         if i%2==1:
            r += i
