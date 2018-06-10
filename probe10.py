0b89489dd08b7027da815436041109efe86089bf
SINAEEE <kimshin1227@gmail.com>
2018-06-10 17:55:21 +0900

    probe commit!

--- probe10.py
+++ probe10.py
@@ -3,12 +3,18 @@ d=dict()
 menu = input('메뉴: ')
 
 if menu == '오뎅':
-    d={'오뎅':300}
+    #d={menu:300}
+    d[menu]=300
 elif menu == '순대':
-    d={'순대':400}
+    #d={menu:400}
+    d[menu]=400
 elif menu == '만두':
-    d={'만두':500}
+    #d={menu:500}
+    d[menu]=500
 else:
     d[menu] = 0
 
 print('가격:{0}'.format(d[menu]))
+
+print()
+
