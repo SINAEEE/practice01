c010340e38348c2be188d261be1718ff0a6f885b
SINAEEE <kimshin1227@naver.com>
2018-06-11 14:04:26 +0900

    Merge remote-tracking branch 'remotes/github/master'
    # Conflicts:
    #	probe04.py

--- probe04.py
+++ probe04.py
@@ -13,7 +13,13 @@ s = """
 </html>"""
 
 
-#print(s.splitlines())
-#print(s.replace('<','').replace('>','').replace('/',''))
-print(s.replace(s[1:49],''))
+
+import re
+
+def remove_tag(content):
+    cleaner = re.compile(('<.*?>'))
+    cleantext = re.sub(cleaner,'',content)
+    return cleantext
+
+print(remove_tag(s))
 
